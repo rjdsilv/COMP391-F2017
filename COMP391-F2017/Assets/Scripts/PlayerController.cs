8fb3361c0f66aec0c736aff1b70e39817720d5c9
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-09-27 11:14:51 -0400

    Implemented player controlls.

---
+++ COMP391-F2017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Like update but used with physics
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Debug.Log("H = " + moveHorizontal + ", V = " + moveVertical);
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        // Debug.Log(movement);
+
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+    }
+}
