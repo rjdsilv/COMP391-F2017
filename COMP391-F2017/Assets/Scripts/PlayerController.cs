28d9d7f470e523b3bed0c49abcc9930288cc3f0e
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-09-27 21:17:09 -0400

    Fixed some properties

--- COMP391-F2017/Assets/Scripts/PlayerController.cs
+++ COMP391-F2017/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,6 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rBody = GetComponent<Rigidbody2D>();
-
     }
 	
 	// Update is called once per frame
